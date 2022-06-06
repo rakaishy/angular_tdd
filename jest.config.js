e62fd6f50e961da288eb957b5adbdcacdaabe34f
rakaishy <alexhander2002@gmail.com>
2022-06-05 22:59:28 -0700

    added config files, babel and git ignore

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular'
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  }
+};
---
+++ src/app/product-list/services/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { sumBy } from 'lodash';
+
+@Injectable()
+export class ProductService {
+  public constructor() { }
+
+  public getTotal(selectedProducts: {id: string, price: number} []): number {
+    return sumBy(selectedProducts, (product) => {
+      return product.price;
+    })
+  }
+}
