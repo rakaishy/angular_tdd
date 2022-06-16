953605aec1d4f7ccb7f63e21caba5493270d621f
rakaishy <alexhander2002@gmail.com>
2022-06-16 15:15:39 -0700

    feat: tdd tests declaration (very basic) implementation on branch "tdd_solved"

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -2,9 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
 import { Product } from '../products';
-import { isEmpty, take } from 'rxjs';
+import { take } from 'rxjs';
 
-describe('ProductService', () => {
+describe('ProductService set' , () => {
   let service: ProductService;
   const mockPorduct: Product = { id: 15, description: 'mock product', name: 'mock me plus', price: 10000 }
 
@@ -13,36 +13,54 @@ describe('ProductService', () => {
     service = TestBed.inject(ProductService);
   });
 
-  it('Service should be created', () => {
-    expect(service).toBeDefined();
-  });
+   it('Service should be created', () => {
+     expect(service).toBeDefined();
+   });
 
-  it('should return the total results', () => {
-    const result = service.getTotal([{ price: 100, amount: 3 }, { price: 10, amount: 10 }]);
-    expect(result).toEqual(400);
-  });
+  describe('Product Service get Totals', () => {
+    it('should return the total results', () => {
+      const result = service.getTotal([
+        { price: 100, amount: 3 },
+        { price: 10, amount: 10 },
+      ]);
+      expect(result).toEqual(400);
+    });
 
-  it('should return the total of zero on empty list', () => {
-    const result = service.getTotal([]);
-    expect(result).toEqual(0);
-  });
+     it('should return the total of zero on empty list', () => {
+       const result = service.getTotal([]);
+       expect(result).toEqual(0);
+     });
 
-  it('should add products', (done) => {
-    service.addProduct(mockPorduct);
-    service.selectedProducts.pipe(take(1)).subscribe((products) => {
-      expect(products).toContain(mockPorduct);
-      done();
+  });
+  
+  describe('Product Service add products', () => {
+    it('should add products', (done) => {
+      service.addProduct(mockPorduct);
+      service.selectedProducts.pipe(take(1)).subscribe((products) => {
+        expect(products).toContain(mockPorduct);
+        done();
+      });
     });
   });
+ 
+  describe('Product Service remove products', () => {
+    it('should remove products', (done) => {
+      // adding and item to be removed next.
+      service.addProduct(mockPorduct);
+      //removing the added product
+      service.removeProduct(mockPorduct);
+      service.selectedProducts.pipe(take(1)).subscribe((products) => {
+        expect(products).not.toContain(mockPorduct);
+        done();
+      });
+    });
 
-  it('should remove products', (done) => {
-    // adding and item to be removed next.
-    service.addProduct(mockPorduct);
-    //removing the added product
-    service.removeProduct(mockPorduct);
-    service.selectedProducts.pipe(take(1)).subscribe((products) => {
-      expect(products).not.toContain(mockPorduct);
-      done();
+    it('should throw error on product not found', () => {
+      try {
+        service.removeProduct(mockPorduct);
+      } catch (error) {
+        expect(error).toBeTruthy();
+      }
     });
   });
 });
